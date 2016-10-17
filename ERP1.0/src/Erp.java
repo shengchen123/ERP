0e07fd2ec032e9da5f6268eed699034e9e67fa8b
shengchen123 <qqcs2g123>
2016-10-17 21:17:19 +0800

    盛晨第一次测试上传代码

---
+++ ERP1.0/src/Erp.java
@@ -0,0 +1,44 @@
+import java.io.Serializable;
+
+/**
+ * Git服务器测试实体类
+ * 
+ * @author shengchen
+ * 
+ */
+public class Erp implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Integer erpId;
+	private String erpName;
+
+	public Integer getErpId() {
+		return erpId;
+	}
+
+	public void setErpId(Integer erpId) {
+		this.erpId = erpId;
+	}
+
+	public String getErpName() {
+		return erpName;
+	}
+
+	public void setErpName(String erpName) {
+		this.erpName = erpName;
+	}
+
+	public Erp() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Erp [erpId=" + erpId + ", erpName=" + erpName + "]";
+	}
+
+}
